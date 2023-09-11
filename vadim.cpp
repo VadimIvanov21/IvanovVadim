a5edfebf016740b5844247c3cd6d790c8c2b92e8
VadimIvanov21 <144548392+VadimIvanov21@users.noreply.github.com>
2023-09-11 10:55:25 +0300

    ererr
    erer

---
+++ vadim.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "vadim13.h"
+
+using namespace std;
+
+void itc_num_print(int number){
+cout << number
+}
---
+++ vadim13.h
@@ -0,0 +1,5 @@
+#ifndef VADIM13_H_INCLUDED
+#define VADIM13_H_INCLUDED
+
+void itc_num_print(int number);
+#endif // VADIM13_H_INCLUDED
