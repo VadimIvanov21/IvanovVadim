5db8a9aa20e272aa48b813f6be4b1dbf45abb731
VadimIvanov21 <144548392+VadimIvanov21@users.noreply.github.com>
2023-09-19 17:33:37 +0300

    new
    121

--- vadim.cpp
+++ vadim.cpp
@@ -3,6 +3,50 @@
 
 using namespace std;
 
-void itc_num_print(int number){
-cout << number
+void itc_name(){
+cout << "VADIM";
+}
+
+void itc_fio(){
+itc_name();
+cout << " " << "IVANOV"<< " " << "SERGEEVICH";
+}
+
+int itc_abs(int num){
+if (num > 0){
+    return num;
+}
+else{
+    return num * -1;
+}
+}
+double itc_fabs(double num){
+if (num > 0){
+    return num;
+}
+else{
+    return num * -1;
+}
+}
+
+
+int itc_revnbr(int num){
+int b, q;
+if(num > 0){
+while (num > 0){
+    b = num % 10;
+    num = num / 10;
+    q = q * 10 + b;
+}
+return q;
+}
+if(num < 0){
+   num = num * -1;
+   while (num > 0){
+    b = num % 10;
+    num = num / 10;
+    q = q * 10 + b;
+}
+return q *-1;
+}
 }
--- vadim13.h
+++ vadim13.h
@@ -1,5 +1,9 @@
 #ifndef VADIM13_H_INCLUDED
 #define VADIM13_H_INCLUDED
-
-void itc_num_print(int number);
+using namespace std;
+void itc_name();
+void itc_fio();
+int itc_abs(int num);
+double itc_fabs(double num);
+int itc_revnbr(int num);
 #endif // VADIM13_H_INCLUDED
