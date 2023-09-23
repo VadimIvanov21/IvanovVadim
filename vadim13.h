64c8265d5aee198b6c5efe098a8e106086940b79
VadimIvanov21 <144548392+VadimIvanov21@users.noreply.github.com>
2023-09-23 11:49:03 +0300

    Update vadim13.h

--- vadim13.h
+++ vadim13.h
@@ -1,9 +1,24 @@
-#ifndef VADIM13_H_INCLUDED
-#define VADIM13_H_INCLUDED
+#ifndef EASY2_H_INCLUDED
+#define EASY2_H_INCLUDED
+
 using namespace std;
 void itc_name();
 void itc_fio();
 int itc_abs(int num);
 double itc_fabs(double num);
 int itc_revnbr(int num);
-#endif // VADIM13_H_INCLUDED
+int itc_sqrt(int a);
+int itc_skv(int a);
+int itc_spr(int a, int b);
+int itc_str(int a, int b, int c);
+double itc_scir(int a);
+bool itc_iseven(int a);
+int itc_max(int a, int b);
+int itc_min(int a, int b);
+double itc_fmax(double a, double b);
+double itc_fmin(double a, double b);
+double itc_pow(int num, int step);
+bool itc_inpositive(int num);
+bool itc_inpositive_d(int num);
+
+#endif // EASY2_H_INCLUDED
